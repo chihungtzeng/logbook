9a3dd701a6005437d63e4ae9eff8141cd448bae7
clark tseng <clark@cs.nthu.edu.tw>
2019-03-01 11:42:45 +0800

    Add gflags example.

---
+++ code/gflags/main.cpp
@@ -0,0 +1,21 @@
+#include <gflags/gflags.h>
+#include <iostream>
+
+using namespace std;
+
+DEFINE_bool(big_menu, true, "Include 'advanced' options in the menu listing");
+DEFINE_string(languages, "english,french,german", "comma-separated list of languages to offer in the 'lang' menu");
+
+int main(int argc, char **argv) {
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  cout << "argc=" << argc << endl;
+  if (FLAGS_big_menu) {
+    cout << "big menu is true" << endl;
+  } else {
+    cout << "big menu is false" << endl;
+  }
+
+  cout << "languages=" << FLAGS_languages << endl;
+  return 0;
+}
